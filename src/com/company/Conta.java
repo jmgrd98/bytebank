d5faaed338e992f6ff901669dde72337e8f4abe8
João Marcelo Dantas <jmgrd98@gmail.com>
2023-06-11 09:04:34 -0300

    abrir e fechar conta methods

--- src/com/company/Conta.java
+++ src/com/company/Conta.java
@@ -9,35 +9,47 @@ public class Conta {
         this.numero = numero;
         this.titular = titular;
         this.saldo = saldo;
-    }
+    };
 
     public int getNumero(int numero) {
         System.out.println("Número: " + this.numero);
         return this.numero;
-    }
+    };
     public void setNumero(int numero) {
         this.numero = numero;
         System.out.println("Número: " + this.numero);
-    }
+    };
     public Cliente getTitular(Cliente titular) {
         System.out.println("Titular: " + this.titular);
         return this.titular;
-    }
-
-
+    };
     public double getSaldo(double saldo) {
         System.out.println("Saldo: " + this.saldo);
         return this.saldo;
-    }
+    };
     public void setSaldo(double saldo) {
         this.saldo = saldo;
         System.out.println("Saldo: " + this.saldo);
-    }
+    };
+
+    public Conta abrirConta(int numero, Cliente titular, double saldo) {
+        Conta conta = new Conta(numero, titular, saldo);
+        System.out.println("Conta aberta com sucesso!");
+        return conta;
+    };
+
+    public void fecharConta() {
+        if (this.saldo == 0) {
+            System.out.println("Conta fechada com sucesso!");
+        } else {
+            System.out.println("Conta não pode ser fechada!");
+        };
+    };
 
     public void deposita(double valor) {
         this.saldo += valor;
         System.out.println("Depósito realizado com sucesso!");
-    }
+    };
 
     public boolean saca(double valor) {
         if (this.saldo >= valor) {
@@ -45,10 +57,10 @@ public class Conta {
             System.out.println("Saque realizado com sucesso!");
             System.out.println("Valor sacado: " + valor);
             return true;
-        }
+        };
         System.out.println("Saldo insuficiente!");
         return false;
-    }
+    };
 
     public boolean transfere(double valor, Conta destino) {
         if (this.saca(valor)) {
@@ -56,10 +68,10 @@ public class Conta {
             System.out.println("Transferência realizada com sucesso!");
             System.out.println("Valor transferido: " + valor);
             return true;
-        }
+        };
         System.out.println("Transferência não realizada!");
         return false;
-    }
+    };
 
 
-}
+};
